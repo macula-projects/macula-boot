918ec3585a00400b8649d99d9aae2a9d0aa0fc34
liangkangzhu <Kangzhu.Liang@infinitus-int.com>
2022-07-26 16:28:07 +0800

    返回结果优化

--- macula-boot-examples/macula-example-consumer/src/main/java/dev/macula/example/consumer/feign/GapiService.java
+++ macula-boot-examples/macula-example-consumer/src/main/java/dev/macula/example/consumer/feign/GapiService.java
@@ -2,14 +2,15 @@ package dev.macula.example.consumer.feign;
 
 import dev.macula.example.consumer.feign.configuration.GapiConfiguration;
 import dev.macula.example.consumer.vo.PoBaseDto;
+import dev.macula.example.consumer.vo.PoBaseResult;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(name = "gapi-service", url = "https://gapi.infinitus.com.cn", configuration = GapiConfiguration.class)
+@FeignClient(name = "gapi-service", url = "https://gapi-dev.infinitus.com.cn", configuration = GapiConfiguration.class)
 public interface GapiService {
 
     @PostMapping("/ecp/po/trade/updateEvaluationStatus")
-    Object updateEvaluationStatus(@RequestBody PoBaseDto poBaseDto);
+    PoBaseResult updateEvaluationStatus(@RequestBody PoBaseDto poBaseDto);
 
 }
