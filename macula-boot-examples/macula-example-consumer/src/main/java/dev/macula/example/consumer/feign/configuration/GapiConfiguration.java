06c5829fab651cb879b31262fc9f524c378567d3
Rain Wang <rainsoft@163.com>
2022-07-27 09:28:17 +0800

    添加KONG API的自动签名拦截器

--- macula-boot-examples/macula-example-consumer/src/main/java/dev/macula/example/consumer/feign/configuration/GapiConfiguration.java
+++ macula-boot-examples/macula-example-consumer/src/main/java/dev/macula/example/consumer/feign/configuration/GapiConfiguration.java
@@ -1,48 +1,18 @@
 package dev.macula.example.consumer.feign.configuration;
 
-import dev.macula.boot.constants.SecurityConstants;
-import feign.RequestInterceptor;
-import feign.RequestTemplate;
-import org.apache.commons.codec.digest.HmacUtils;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.time.ZonedDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.Locale;
+import dev.macula.boot.starter.cloud.feign.interceptor.KongApiInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 
-import static java.time.ZoneOffset.UTC;
-import static org.apache.commons.codec.binary.Base64.encodeBase64String;
-import static org.apache.commons.codec.digest.HmacAlgorithms.HMAC_SHA_256;
+public class GapiConfiguration {
+    @Value("${gapi.username}")
+    private String username;
+    @Value("${gapi.secret}")
+    private String secret;
 
-public class GapiConfiguration implements RequestInterceptor {
-
-    // 消费者名称与密钥
-    private static final String CUSTOMER_USERNAME = "applicationA";
-    private static final String CUSTOMER_SECRET = "0UXNHZGKPUNUIJMVQRURULAEG2M0Y96R";
-    private static final DateTimeFormatter RFC_7231_FORMATTER = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss O").withLocale(Locale.ENGLISH);
-
-    @Override
-    public void apply(RequestTemplate requestTemplate) {
-        // 时间
-        String date = RFC_7231_FORMATTER.format(ZonedDateTime.now(UTC));
-
-        // 请求体摘要
-        MessageDigest messageDigest = null;
-        try {
-            messageDigest = MessageDigest.getInstance("SHA-256");
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-        byte[] hash = messageDigest.digest(requestTemplate.body());
-        String digest = "SHA-256=" + encodeBase64String(hash);
-        // 签名，计算方法为 hmac_sha_256(key, 日期 + 方法 + 路径 + 摘要)
-        HmacUtils hmacSha256 = new HmacUtils(HMAC_SHA_256, CUSTOMER_SECRET);
-        String signature = encodeBase64String(hmacSha256.hmac("date: " + date + "\n" + requestTemplate.method() + " " + requestTemplate.url() + " HTTP/1.1\ndigest: " + digest));
-        // HMAC 授权
-        String authorization = "hmac username=\"" + CUSTOMER_USERNAME + "\", algorithm=\"hmac-sha256\", headers=\"date request-line digest\", signature=\"" + signature + "\"";
-
-        requestTemplate.header(SecurityConstants.AUTHORIZATION_KEY, authorization);
-        requestTemplate.header("Date", date);
-        requestTemplate.header("Digest", digest);
+    @Bean
+    KongApiInterceptor kongApiInterceptor() {
+        return new KongApiInterceptor(username, secret);
     }
+
 }
