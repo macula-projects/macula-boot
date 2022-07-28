7e2a94af377be34cd5cb03321b04281c1bc03ca6
Rain Wang <rainsoft@163.com>
2022-07-29 00:26:33 +0800

    添加脱敏的JSON注解

---
+++ macula-boot-starters/macula-boot-starter-web/src/main/java/dev/macula/boot/starter/web/annotation/Sensitive.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2022 Macula
+ *   macula.dev, China
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.macula.boot.starter.web.annotation;
+
+import com.fasterxml.jackson.annotation.JacksonAnnotationsInside;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import dev.macula.boot.starter.web.json.SensitiveSerializer;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * {@code Sensitvie} 脱敏的JSON注解
+ *
+ * @author rain
+ * @since 2022/7/28 23:48
+ */
+@Target({ElementType.FIELD, ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@JacksonAnnotationsInside
+@JsonSerialize(using = SensitiveSerializer.class)
+public @interface Sensitive {
+    /**
+     * 脱敏的类型,默认手机号
+     *
+     * @return
+     */
+    Type value();
+
+    /**
+     * CUSTOM_HIDE/CUSTOM_OVERLAY 时生效
+     * 开始位置（包含）
+     *
+     * @return
+     */
+    int startInclude() default 0;
+
+    /**
+     * CUSTOM_HIDE/CUSTOM_OVERLAY 时生效
+     * 结束位置（不包含）
+     *
+     * @return
+     */
+    int endExclude() default 0;
+
+
+    /**
+     * CUSTOM_OVERLAY 时生效,*重复的次数
+     *
+     * @return
+     */
+    int overlayRepeat() default 4;
+
+
+    /**
+     * Enumeration used with {@link Sensitive}
+     */
+    public enum Type {
+        /**
+         * 手机号
+         */
+        MOBILE,
+        /**
+         * 中文名
+         */
+        CHINESE_NAME,
+        /**
+         * 身份证号
+         */
+        ID_CARD,
+        /**
+         * 座机号
+         */
+        FIXED_PHONE,
+        /**
+         * 地址
+         */
+        ADDRESS,
+        /**
+         * 电子邮件
+         */
+        EMAIL,
+        /**
+         * 银行卡
+         */
+        BANK_CARD,
+        /**
+         * 自定义，有多少个字符替换成多少个*
+         * e.g: startInclude=3,endExclude=7,隐藏第3个到第7个的字符
+         */
+        CUSTOM_HIDE,
+        /**
+         * 保留方式隐藏
+         * e.g: startInclude=3,endExclude=4 ,保留前面3个和后面的4个
+         */
+        CUSTOM_RETAIN_HIDE,
+        /**
+         * 自定义,只替换成指定个*
+         */
+        CUSTOM_OVERLAY,
+    }
+}
---
+++ macula-boot-starters/macula-boot-starter-web/src/main/java/dev/macula/boot/starter/web/json/SensitiveSerializer.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2022 Macula
+ *   macula.dev, China
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package dev.macula.boot.starter.web.json;
+
+import cn.hutool.core.util.ObjectUtil;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.BeanProperty;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.ContextualSerializer;
+import dev.macula.boot.starter.web.annotation.Sensitive;
+import dev.macula.boot.starter.web.utils.SensitiveUtil;
+
+import java.io.IOException;
+import java.util.Objects;
+
+/**
+ * {@code SensitiveSerializer} 脱敏JSON序列号
+ *
+ * @author rain
+ * @since 2022/7/28 23:53
+ */
+public class SensitiveSerializer extends JsonSerializer<String> implements ContextualSerializer {
+    private Sensitive.Type type;
+    private int startInclude;
+    private int endExclude;
+    private int overlayRepeat;
+
+    public SensitiveSerializer() {
+    }
+
+    public SensitiveSerializer(final Sensitive sensitive) {
+        this.type = sensitive.value();
+        this.startInclude = sensitive.startInclude();
+        this.endExclude = sensitive.endExclude();
+        this.overlayRepeat = sensitive.overlayRepeat();
+    }
+
+
+    @Override
+    public void serialize(String value, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        if (ObjectUtil.isEmpty(value)) {
+            gen.writeString(value);
+            return;
+        } else {
+            switch (this.type) {
+                case MOBILE:
+                    gen.writeString(SensitiveUtil.handlerMobile(value));
+                    break;
+                case ID_CARD:
+                    gen.writeString(SensitiveUtil.handlerIdCard(value));
+                    break;
+                case BANK_CARD:
+                    gen.writeString(SensitiveUtil.handlerBankCard(value));
+                    break;
+                case CHINESE_NAME:
+                    gen.writeString(SensitiveUtil.handlerUsername(value));
+                    break;
+                case FIXED_PHONE:
+                    gen.writeString(SensitiveUtil.handlerFixedPhone(value));
+                    break;
+                case ADDRESS:
+                    gen.writeString(SensitiveUtil.handlerAddress(value));
+                    break;
+                case EMAIL:
+                    gen.writeString(SensitiveUtil.handlerEmail(value));
+                    break;
+                case CUSTOM_HIDE:
+                    gen.writeString(SensitiveUtil.hide(value, startInclude, endExclude));
+                    break;
+                case CUSTOM_RETAIN_HIDE:
+                    gen.writeString(SensitiveUtil.hide(value, startInclude, (value.length() - endExclude)));
+                    break;
+                case CUSTOM_OVERLAY:
+                    gen.writeString(SensitiveUtil.overlay(value, SensitiveUtil.ASTERISK, overlayRepeat, startInclude, endExclude));
+                    break;
+                default:
+                    gen.writeString(value);
+            }
+        }
+
+    }
+
+    @Override
+    public JsonSerializer<?> createContextual(SerializerProvider prov, BeanProperty property) throws JsonMappingException {
+        if (Objects.isNull(property)) {
+            return prov.getDefaultNullValueSerializer();
+        }
+        if (Objects.equals(property.getType().getRawClass(), String.class)) {
+            Sensitive sensitive = property.getAnnotation(Sensitive.class);
+            if (Objects.isNull(sensitive)) {
+                sensitive = property.getContextAnnotation(Sensitive.class);
+            }
+            if (Objects.nonNull(sensitive)) {
+                return new SensitiveSerializer(sensitive);
+            }
+        }
+        return prov.findValueSerializer(property.getType(), property);
+    }
+}
