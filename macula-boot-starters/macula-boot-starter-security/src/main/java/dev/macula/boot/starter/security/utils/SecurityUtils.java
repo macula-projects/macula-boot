568d17eef0728830fa17ab63a9ef98664436bc69
Rain Wang <rainsoft@163.com>
2022-12-08 21:31:19 +0800

    ddd目录问题修复

--- macula-boot-starters/macula-boot-starter-security/src/main/java/dev/macula/boot/starter/security/utils/SecurityUtils.java
+++ macula-boot-starters/macula-boot-starter-security/src/main/java/dev/macula/boot/starter/security/utils/SecurityUtils.java
@@ -17,7 +17,18 @@
 
 package dev.macula.boot.starter.security.utils;
 
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.core.convert.Convert;
+import cn.hutool.core.util.StrUtil;
+import dev.macula.boot.constants.GlobalConstants;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * {@code SecurityUtils} 安全助手
@@ -33,5 +44,103 @@ public class SecurityUtils {
             return SecurityContextHolder.getContext().getAuthentication().getName();
         }
         return null;
+    }
+
+    /**
+     * 获取用户ID
+     *
+     * @return
+     */
+    public static Long getUserId() {
+        Long userId = Convert.toLong(getTokenAttributes().get("userId"));
+        return userId;
+    }
+
+    /**
+     * 获取会员ID
+     *
+     * @return
+     */
+    public static Long getMemberId() {
+        Long userId = Convert.toLong(getTokenAttributes().get("memberId"));
+        return userId;
+    }
+
+    /**
+     * 获取用户登录名
+     *
+     * @return username
+     */
+    public static String getUsername() {
+        String username = Convert.toStr(getTokenAttributes().get("username"));
+        return username;
+    }
+
+    /**
+     * 获取用户昵称/姓名
+     *
+     * @return nickname
+     */
+    public static String getNickname() {
+        String nickname = Convert.toStr(getTokenAttributes().get("nickname"));
+        return nickname;
+    }
+
+    /**
+     * 获取用户角色
+     *
+     * @return 角色Code集合
+     */
+    public static Set<String> getRoles() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && CollectionUtil.isNotEmpty(authentication.getAuthorities())) {
+            Set<String> roles = authentication.getAuthorities()
+                    .stream()
+                    .map(item -> StrUtil.removePrefix(item.getAuthority(), "ROLE_"))
+                    .collect(Collectors.toSet());
+            return roles;
+        }
+        return Collections.EMPTY_SET;
+    }
+
+    /**
+     * 获取部门ID
+     *
+     * @return deptId
+     */
+    public static Long getDeptId() {
+        Long deptId = Convert.toLong(getTokenAttributes().get("deptId"));
+        return deptId;
+    }
+
+    /**
+     * 获取数据权限
+     *
+     * @return DataScope
+     */
+    public static Integer getDataScope() {
+        Integer dataScope = Convert.toInt(getTokenAttributes().get("dataScope"));
+        return dataScope;
+    }
+
+    /**
+     * 判断用户是否为超级管理员
+     *
+     * @return true/false
+     */
+    public static boolean isRoot() {
+        return getRoles().contains(GlobalConstants.ROOT_ROLE_CODE);
+    }
+
+    public static Map<String, Object> getTokenAttributes() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null) {
+            if (authentication instanceof JwtAuthenticationToken) {
+                JwtAuthenticationToken jwtAuthenticationToken = (JwtAuthenticationToken) authentication;
+                Map<String, Object> tokenAttributes = jwtAuthenticationToken.getTokenAttributes();
+                return tokenAttributes;
+            }
+        }
+        return Collections.EMPTY_MAP;
     }
 }
